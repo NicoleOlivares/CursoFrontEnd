6ded8932e19f5d8608e41a0c56f1cae59c094eeb
Nicole Olivares <nicoleolivaresv@gmail.com>
2022-08-24 21:34:35 -0400

    sesion 4 y 5

--- Clases/SESION-2/js/script.js
+++ Clases/SESION-2/js/script.js
@@ -23,13 +23,100 @@ window.addEventListener('DOMContentLoaded', (e) => {
     let boton = document.getElementById("btn-suscribir");
     boton.addEventListener("click", (ev) => {
 
-        //recuperar los valores del formulario
-        let nombre = document.getElementById("nombre").value;
-        console.log("El nombre del suscriptor es: " + nombre);
-    });
+        //bloque try
+        try {
 
+            //recuperar los valores del formulario
+            let nombre = document.getElementById("nombre").value;
+            let email = document.getElementById("correo").value;
+
+            //declarando variable para generar código
+            let genero = getGenero();
+            let intereses = getIntereses();
+
+            //objeto JSON
+            let suscriptor = { //JSON = Javascript object notation
+                //clave: valor
+                nombre_completo: nombre,
+                email,//es lo mismo que email: email,
+                genero,
+                intereses,
+                fecha_registro: (new Date()).toISOString()
+            };
+            //console.log("El nombre del suscriptor es: " + nombre);
+            console.dir(suscriptor);
+            guardarSuscriptor(suscriptor);
+            mostrarExito("Se guardó correctamente su suscripción");
+        } catch(e) {
+            mostrarError(e.message);
+        }
+    });
 });
 
+function mostrarExito (mensaje) {
+    alert(mensaje);
+    //tarea: hacer que el mensaje se muestre en el cuadro verde
 
+}
+
+async function guardarSuscriptor(suscriptor) {
+    const url = "https://curso-frontend-96395-default-rtdb.firebaseio.com/suscriptores.json";
+    /*alternativas:
+    1) Callbacks
+    fetch(url, {
+        method: "POST",
+        body: JSON.stringify(suscriptor)
+    })
+        .then( respuesta => respuesta.json()) //devuelve promesa
+        .then( data => mostrarExito("Se guardó correctamente su suscripción"))
+    ;
+    */
+    //2) Async Await
+    const respuesta = await fetch(url, {
+        method: "POST",
+        body: JSON.stringify(suscriptor)
+    });
+    const data = await respuesta.json();
+    mostrarExito("Se guardó correctamente su suscripción");
+}
+
+function getIntereses() {
+    let inputIntereses = document.querySelectorAll("input[name='intereses']:checked");
+    //lista para guardar los diferentes intereses
+    let arrIntereses = [];
+
+    //alternativa 1:
+    //inputIntereses.forEach(nodoInteres => arrIntereses.push(nodoInteres.value));
+
+    //alternativa 2:
+    for (let i = 0; i < inputIntereses.length; i++) {
+        const interes = inputIntereses[i].value;
+        arrIntereses.push(interes);
+    }
+
+    if (inputIntereses.length < 1) {
+        throw new Error("Debe seleccionar al menos 1 tema de su interés!");
+    }
+    return arrIntereses;
+}
+
+function getGenero() {
+    let inputSeleccionado = document.querySelector("input[name='genero']:checked")
+    if (inputSeleccionado == null) {
+        throw new Error("Debe seleccionar un género!");
+    }
+    const genero = inputSeleccionado.value;
+    return genero;
+}
+
+function mostrarError(mensajeDeError) {
+    //valor anterior "none"
+    document.getElementById("form-mensaje-error").style.display = "block";
+    const ul = document.querySelector("#form-mensaje-error ul");
+    const li = document.createElement("li");
+    const liText = document.createTextNode(mensajeDeError);
+    li.appendChild(liText);
+    ul.appendChild(li);
+}
 
 console.log("Hola mundo!!");
